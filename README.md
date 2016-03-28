f28bb239dd94571417ec74373e3ae283aa1576f7
hopeful0 <hopeful_world@163.com>
2016-03-28 08:08:13 +0800

    change the algorithm

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # exp_surfacetension
-物理力热试验：表面张力的测定实验数据处理程序。
-The program used to process the data of the physics thermal experiment:Determination of surface tension.
+物理力热试验：表面张力系数的测定实验数据处理程序。
+The program used to process the data of the physics thermal experiment:Determination of surface tension ratio.
 # 作者(author)
 hopeful from LZU
 # 日期(date)
 2015.03.24
 # 简介(brief)
+本程序为处理表面张力系数的测定实验的数据而设计
+程序分为3个模块：
+1.传感器灵敏度的测量
+依次输入在0.5g、1.0g、1.5g、2.0g、2.5g、3.0g的情况下，传感器的示数(输入单位为mV)。根据公式F=cU,利用最小二乘法线性拟合，输出灵敏度系数c(单位N/mV),系数k(=1/c,单位mV/N),c的(A类)不确定度U(c)与相关系数r
+2.环的直径的测量
+分别输入测得的环的外径和内径的数据(单位cm),查表并输入测量过程中的t因子(最好保证外径内径数据一致)和测量工具游标卡尺的极限误差(单位cm),程序将输出外径与内径的平均值(D1、D2,单位cm)和其不确定度U(D1)、U(D2)
+3.表面张力系数的测量
+输入通过1、2得到的结果，c、U(c)、D1、U(D1)、D2、U(D2),然后依次输入测量的U1、U2(单位mV),每输入一组数据，将输出通过改组数据得到的deltaU(=U2-U1)、表面张力f和表面张力系数sigma，然后输入测量U1、U2的t因子及极限误差，最后程序将输出由此得到的最终的表面张力系数sigma的平均值与不确定度(单位：N/m)
--- source/surfacetension.cpp
+++ source/surfacetension.cpp
@@ -93,6 +93,12 @@ double getUsigma(ldouble sigmas, double* p, double* Up)
 	return avgsigma * Esigma;
 }
 
+int over()
+{
+	printf("程序运行结束，感谢使用！\n\n");
+	return 0;
+}
+
 int main()
 {
 	printf("欢迎使用 表面张力系数数据处理程序!\n作者：hopeful from LZU\n");
@@ -121,7 +127,7 @@ int main()
 					ys.push_back(temp);
 				double k,b,Sk,Sb,S,r;
 				ls(xs,ys,k,b,Sk,Sb,S,r);
-				printf("输出(c = 1 / k)：\nc = %lf\nk = %lf\nU(c) = %lf\nr = %lf\n",k,1/k,Sk,r);
+				printf("输出(c = 1 / k)：\nc = %lf\nk = %lf\nU(c) = %lf\nr = %lf\n",k * 100,1/(k * 100),Sk * 100,r);
 				break;
 			}
 			case 2:
@@ -131,6 +137,7 @@ int main()
 				printf("依次输入n组外径值D1，输入q结束：\n");
 				while(1 == scanf("%lf",&temp))
 					od.push_back(temp);
+				clear_stdin();
 				printf("依次输入n组内径值D2，输入q结束：\n");
 				while(1 == scanf("%lf",&temp))
                                         id.push_back(temp);
@@ -154,16 +161,17 @@ int main()
 				printf("输入环的内径及其不确定度：\n");
 				scanf("%lf %lf",&p[2],&Up[2]);
 				printf("依次输入n组U1、U2的值，用空格隔开，输入q结束：\n");
-				while(1 > scanf("%lf %lf",&temp1,&temp2))
+				while(2 == scanf("%lf %lf",&temp1,&temp2))
 				{
 					U1.push_back(temp1);
 					U2.push_back(temp2);
 					double dU = temp2 - temp1;
-					double f = dU / p[0];
+					double f = dU * p[0];
 					double s = f / pi / (p[1]+p[2]);
-					printf("deltaU = %lf\nF = %lf\nsigma = %lf\n",dU,f,s);
+					printf("deltaU = %lf\nF = %lf\nsigma = %lf\n",dU,f / 100000,s / 100);
 					sigma.push_back(s);
 				}
+				clear_stdin();
 				double t,di;
 				printf("输入测量U1、U2时的t因子和极限误差：\n");
 				scanf("%lf %lf",&t,&di);
@@ -171,16 +179,17 @@ int main()
 				Up[3] = getU(U1,t,di);
 				p[4] = avg(U2);
 				Up[4] = getU(U2,t,di);
-				printf("sigma = %lf,U(sigma) = %lf\n",avg(sigma),getUsigma(sigma,p,Up));
+				printf("sigma = %lf,U(sigma) = %lf\n",avg(sigma) / 100,getUsigma(sigma,p,Up) / 100);
 				break;
 			}
 			default:
-				return 0;
+				return over();
 		}
 		printf("按回车键继续");
 		clear_stdin();
 		getchar();
+		clear_stdin();
 	}
-	return 0;
+	return over();
 }
 
